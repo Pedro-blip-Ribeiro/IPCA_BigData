333f9c367eca09500b6d5e1467e7e26de8ecb18f
Pedro-blip-Ribeiro <pedrodinoribeiro@gmail.com>
2024-11-12 00:16:33 -0300

    Primeira alteração foi feita no arquivo 'databd.py' onde trocamos a API do twitter por uma API do IBGE e traz a variação do IPCA ao longo do anos, fiz a conexão com o banco e armazenei os dados dentro. Segunda alteração foi feita no arquivo 'graficosprojeto.ipynb' onde trocamos para um gráfico de linhas, alteramos o eixo X e Y, modificamos o Widget de Seleção, Modificação na preparação dos Dados e ajute nas Funções

--- databd.py
+++ databd.py
@@ -1,47 +1,21 @@
+import pandas as pd
 from pymongo import MongoClient
-from dotenv import load_dotenv
-import tweepy
-import os
-import json
 
-# Carregar as variáveis de ambiente do arquivo .env
-load_dotenv()
+#Conectar ao MongoDB
+client = MongoClient('mongodb://localhost:27017')
+db = client['big_data_ibge'] # Nome do Banco de dados
+collection = db['inflacao']
 
-# Configurações da API do Twitter.
-bearer_token = os.getenv('BEARER_TOKEN')
+# Ler o arquivo CSV
+csv_ano = pd.read_csv(r'c:\Users\Lenovo\OneDrive\Documents\IPCA.prn', delimiter="\s+")
+csv_mes_ano = pd.read_csv(r'c:\Users\Lenovo\Downloads\20241111093200.csv')
 
-# Conexão com o MongoDB
-mongo_client = MongoClient('mongodb://localhost:27017/')
-db = mongo_client['bd_api']
-collection = db['tweets'] # Nome da coleção onde os tweets serão armazenados.
+# Converte os DataFrames para dicionários
+data_ano = csv_ano.to_dict(orient='records')
+data_mes_ano = csv_mes_ano.to_dict(orient='records')
 
-# Autenticação com a API do Twitter
-twitter_client = tweepy.Client(bearer_token=bearer_token)
+# Inserir dados no MongoDB
+collection.insert_many(data_ano)
+#collection.insert_many(data_mes_ano)
 
-# Palavras-chave para buscar
-keywords = ['Inteligência Artificial', '#InteligenciaArtificial', '#AI']
-
-# Função para coletar tweets
-def collect_tweets(keywords):
-    for keywords in keywords:
-        print(f"Coletando tweets sobre: {keywords}")
-        # Quantidade e tipo a serem coletados
-        tweets = twitter_client.search_recent_tweets(query=keywords, max_results=10, tweet_fields=['created_at', 'author_id', 'public_metrics'])
-
-        if tweets.data:
-            for tweet in tweets.data:
-            # Estrutura para armazenar dados do tweet
-                tweet_data = {
-                    'text': tweet.text,
-                    'created_at': tweet.created_at,
-                    'author_id': tweet.author_id,
-                    'public_metrics': tweet.public_metrics
-                }
-            # Salva no MongoDB
-                collection.insert_one(tweet_data)
-                print(f"Tweet salvo: {tweet.text}")
-        else:
-            print("Nenhum tweet encontrado com a palavra-chave.")
-
-# Executar a coleta
-collect_tweets(keywords)
+print("Dados importados com sucesso!")
