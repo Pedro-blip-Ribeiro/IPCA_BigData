622d6ec8db03b756523a17e972538afbf87223ad
Pedro-blip-Ribeiro <pedrodinoribeiro@gmail.com>
2024-11-19 10:14:31 -0300

    Versão do código com o banco corrigído.

--- databd.py
+++ databd.py
@@ -1,13 +1,17 @@
 import pandas as pd
+import pymongo
 from pymongo import MongoClient
 
 #Conectar ao MongoDB
-client = MongoClient('mongodb://localhost:27017')
-db = client['big_data_ibge'] # Nome do Banco de dados
-collection = db['inflacao']
+try:
+    client = MongoClient('mongodb+srv://pedrotechribeiro:pedro2501@cluster0.z6kzj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    db = client['big_data_ibge'] # Nome do Banco de dados
+    collection = db['inflacao']
+except Exception as e:
+    print(f"Erro ao se conectar ao MongoDB: {e}")
 
 # Ler o arquivo CSV
-csv_ano = pd.read_csv(r'c:\Users\Lenovo\OneDrive\Documents\IPCA.prn', delimiter="\s+")
+csv_ano = pd.read_csv(r'c:\Users\Lenovo\OneDrive\Documents\IPCA.prn', delimiter=r"\s+")
 csv_mes_ano = pd.read_csv(r'c:\Users\Lenovo\Downloads\20241111093200.csv')
 
 # Converte os DataFrames para dicionários
